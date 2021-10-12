a198493a5f9e0ee0cf3dbcdf0316e3b1d7112367
Mateusz "mat" Rumian <58699800+mat-rumian@users.noreply.github.com>
2021-10-12 12:21:22 +0200

    Update img build registry

--- opentelemetry-java-instrumentation-jar/image-build-and-push.sh
+++ opentelemetry-java-instrumentation-jar/image-build-and-push.sh
@@ -2,18 +2,18 @@
 set -e
 
 VERSION="1.6.2"
-AWS_ACCOUNT_NUMBER="663229565520"
-AWS_REGION=us-west-2
-REGISTRY="${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
+AWS_REGION="us-east-1"
 # TODO Change "a4t4y2n3" to "sumologic" after alias migration in the AWS ECR
-REPOSITORY="a4t4y2n3/opentelemetry-java-instrumentation-jar"
+REGISTRY="public.ecr.aws/a4t4y2n3"
+REPOSITORY="opentelemetry-java-instrumentation-jar"
 
 VERSION_TAG="${REGISTRY}/${REPOSITORY}:${VERSION}"
 LATEST_TAG="${REGISTRY}/${REPOSITORY}:latest"
 
 echo "Login to AWS ECR"
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${REGISTRY}
+aws ecr-public get-login-password --region ${AWS_REGION} \
+	| docker login --username AWS --password-stdin ${REGISTRY}
 
 echo "Building version ${VERSION}"
 docker build --build-arg INSTRUMENTATION_VERSION=${VERSION} -t "${VERSION_TAG}" .
